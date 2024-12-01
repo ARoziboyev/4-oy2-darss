36b2f4f3beb1cb2ab285d4a7767f9c2dad7026ae
ARoziboyev <arozibpyev212@gmail.com>
2024-12-01 21:44:51 +0500

    первый коммит

---
+++ main.js
@@ -0,0 +1,492 @@
+// 1.Berilgan son musbat bo‘lsa, 1 chiqarilsin.
+
+
+
+// let num = +prompt("sonni kiriting ");
+
+// if (num > 0){
+//     console.log("1");
+// }
+// else{
+//     console.log("musbat son kiritinng");
+    
+// }
+
+// 2.Agar son manfiy bo‘lsa, -1 chiqarilsin, aks holda 0 chiqarilsin.
+
+
+// let num = +prompt("sonni kiriting");
+// if (num > 0){
+//     console.log("-1");
+// }else{
+//     console.log("0");
+    
+// }
+
+
+
+// 3.Agar son 0 ga teng bo‘lsa, 1 chiqarilsin, aks holda 0 chiqarilsin.
+ 
+
+// let num = +prompt("sonni kiriting");
+// if (num == 0) {
+//     console.log("1");
+// }
+// else{
+//     console.log("0");
+    
+// }
+
+
+// 4.Son musbat va 100 dan katta bo‘lsa, 1 chiqarilsin.
+
+
+// let num = +prompt("sonni kiriting");
+
+// if (num == 0 && num > 100 ){
+//     console.log("1");
+// }
+
+// else{
+//     console.log("son musbat emas");
+// }
+
+// 5.Agar son manfiy va 10 dan kichik bo‘lsa, -1 chiqarilsin.
+
+
+// let num = +prompt("sonni kiriting");
+// if (num < 0 && num <= 10 ) {
+//     console.log("-1");
+// }else{
+//     console.log("kiritilgan son noto'g'ri");  
+// }
+
+
+// 6.Son musbat bo‘lsa, uning kvadrati chiqarilsin, aks holda 0 chiqarilsin.
+
+
+// let num = prompt("Sonni kiriting");
+
+// if (num > 0) { 
+//     console.log(num * num);
+// } else {
+//     console.log("0");}
+
+
+// 7.Agar son manfiy bo‘lsa, uning absolyut qiymati chiqarilsin.
+
+
+
+// let num = +prompt("Sonni kiriting"); 
+
+// if (num < 0) {
+//     console.log(Math.abs(num));  
+// } else {
+//     console.log(num); 
+// }
+
+
+
+// 8.Son nolga teng bo‘lsa, 10 chiqarilsin, aks holda sonning o‘zi chiqarilsin.
+
+
+
+// let num = +prompt("Sonni kiriting"); 
+
+// if (num === 0) { 
+//     console.log(10);
+// } else {
+//     console.log(num);
+// }
+
+
+// 9.Agar son 1 dan katta bo‘lsa, sonning kubi chiqarilsin.
+
+
+// let num = prompt("Sonni kiriting");
+
+//  if (num > 0) { 
+//      console.log(num * num *num);
+//  } else {
+//    console.log("0");}
+
+// 10.Son 0 dan kichik bo‘lsa, -10 chiqarilsin, aks holda 10 chiqarilsin.
+
+
+// let num = +prompt("sonni kiriting");
+
+// if (num < 0) {
+//     console.log("-10");
+// }else{
+//     console.log("10");
+    
+// }
+
+
+// 11.Agar son juft bo‘lsa, 2 chiqarilsin, aks holda 1 chiqarilsin.
+
+
+
+// let num = +prompt("sonni kiriting")
+
+
+// if (num % 2 === 0) {  
+//     console.log("2"); 
+// } else {
+//     console.log("1");  
+// }
+
+
+// 12.Agar son toq bo‘lsa, 3 chiqarilsin, aks holda 4 chiqarilsin
+
+
+// let num = +prompt("sonni kiriting")
+
+// if (num % 2 === 1) {  
+//     console.log("3"); 
+// } else {
+//     console.log("4");  
+// }
+
+
+// 13.Son juft va 10 dan katta bo‘lsa, 1 chiqarilsin
+
+// let num = +prompt("sonni kiriting")
+
+// if (num % 2 === 0 && num > 10) {
+//     console.log("1");
+// }else {
+//     console.log("Shart bajarilmadi");
+// }
+
+
+// 14.Agar son toq va manfiy bo‘lsa, -1 chiqarilsin
+
+// let num = +prompt("sonni kiriting")
+
+// if (num % 2 !== 0 && num < 0) {
+//     console.log("-1");
+// }else {
+//     console.log("Shart bajarilmadi");
+// }
+
+
+
+// 15.Son juft va 5 ga bo‘linadigan bo‘lsa, 100 chiqarilsin.
+
+// let num = +prompt("sonni kiriting")
+
+// if (num % 2 === 0 && num % 5 === 0) {
+//     console.log("100");
+// }else {
+//     console.log("Shart bajarilmadi");
+// }
+
+// 16.Agar son juft bo‘lmasa, sonning kvadrati chiqarilsin.
+
+
+// let num = +prompt("sonni kiriting")
+
+// if (num % 2 !== 0 ) {
+//     console.log(num * num);
+    
+// }
+
+// 17.Son juft va musbat bo‘lsa, uning kubi chiqarilsin.
+
+// let num = +prompt("sonni kiriting")
+
+// if (num % 2 === 0) {
+//     console.log(num * num *num);
+    
+// }
+
+// 18.Agar son juft va 20 dan katta bo‘lsa, 50 chiqarilsin, aks holda -50 chiqarilsin.
+
+// let num = +prompt("sonni kiriting")
+// if (num % 2 === 0 && num > 20) {
+//     console.log("50");
+// }else{
+//     console.log("-50");
+// }
+
+
+// 19.Son toq va 7 ga bo‘linadigan bo‘lsa, 7 chiqarilsin.
+
+// let num = +prompt("sonni kiriting")
+
+// if (num % 2 !== 0 && num % 7 === 0) {
+//     console.log("7");
+// }else {
+//     console.log("Shart bajarilmadi");
+// }
+
+
+// 20.Agar son juft va manfiy bo‘lsa, -20 chiqarilsin.
+
+// let num = +prompt("sonni kiriting")
+// if (num % 2 === 0 && num < 0) {
+//     console.log("-20");
+// }else {
+//     console.log("Shart bajarilmadi");
+// }
+
+
+// 21.Ikki son teng bo‘lsa, 1 chiqarilsin, aks holda 0 chiqarilsin
+
+// let num1 = +prompt("Birinchi sonni kiriting");
+// let num2 = +prompt("Ikkinchi sonni kiriting");
+
+// num1 = Number(num1);
+// num2 = Number(num2);
+
+// if (num1 === num2) {
+//     console.log("1");
+// } else {
+//     console.log("0");
+// }
+
+
+// 22.Agar birinchi son ikkinchisidan katta bo‘lsa, 2 chiqarilsin, aks holda 3 chiqarilsin.
+
+
+// let num1 = +prompt("Birinchi sonni kiriting");
+// let num2 = +prompt("Ikkinchi sonni kiriting");
+
+// num1 = Number(num1);
+// num2 = Number(num2);
+
+// if (num1 > num2) {
+//     console.log("2");
+// } else {
+//     console.log("3");
+// }
+
+
+// 23.Birinchi son ikkinchisidan kichik va musbat bo‘lsa, 10 chiqarilsin
+
+// let num1 = +prompt("Birinchi sonni kiriting");
+// let num2 = +prompt("Ikkinchi sonni kiriting");
+
+// num1 = Number(num1);
+// num2 = Number(num2);
+
+// if (num1 < num2 && num1 > 0) {
+//     console.log("10");
+// } else {
+//     console.log("noto'g'ri son kiritdingiz");
+// }
+
+// 24.Agar birinchi son ikkinchisidan katta va toq bo‘lsa, 5 chiqarilsin.
+
+// let num1 = +prompt("Birinchi sonni kiriting");
+// let num2 = +prompt("Ikkinchi sonni kiriting");
+
+// num1 = Number(num1);
+// num2 = Number(num2);
+
+// if (num1 > num2 && num1 % 2 !== 0) {
+//     console.log("5");
+// } else {
+//     console.log("noto'g'ri son kiritdingiz");
+// }
+
+
+// 25.Ikkala son ham musbat bo‘lsa, ularning yig‘indisi chiqarilsin.
+
+// let num1 = +prompt("Birinchi sonni kiriting");
+// let num2 = +prompt("Ikkinchi sonni kiriting");
+
+// if (num1 > 0 && num2 > 0) {
+//     console.log(num1 + num2);
+// } else {
+//     console.log("Ikkala son ham musbat emas");
+// }
+
+
+// 26.Agar birinchi son ikkinchisidan kichik va ikkinchi son juft bo‘lsa, -1 chiqarilsin.
+
+// let num1 = +prompt("Birinchi sonni kiriting");
+// let num2 = +prompt("Ikkinchi sonni kiriting");
+
+// if (num1 < num2 && num2 % 2 === 0) {
+//     console.log("-1"); 
+// }
+
+// 27.Ikkala son teng bo‘lsa, 100, aks holda -100 chiqarilsin.
+
+
+// let num1 = +prompt("Birinchi sonni kiriting");
+// let num2 = +prompt("Ikkinchi sonni kiriting");
+
+// num1 = Number(num1);
+// num2 = Number(num2);
+
+// if (num1 === num2) {
+//     console.log("100");
+// } else {
+//     console.log("-100");
+// }
+
+// 28.Birinchi son 10 va ikkinchi son 5 ga bo‘linadigan bo‘lsa, 50 chiqarilsin.
+
+// let num1 = +prompt("Birinchi sonni kiriting");
+// let num2 = +prompt("Ikkinchi sonni kiriting");
+
+// num1 = Number(num1);
+// num2 = Number(num2);
+
+// if (num1 % 10 === 0 && num2 % 5 === 0) {
+//     console.log("50");
+// } else {
+//     console.log("noto'g'ri son kiritdingiz");
+// }
+
+// 29.Agar birinchi son ikkinchisidan 2 marta katta bo‘lsa, 2 chiqarilsin.
+
+// let num1 = +prompt("Birinchi sonni kiriting");
+// let num2 = +prompt("Ikkinchi sonni kiriting");
+
+// num1 = Number(num1);
+// num2 = Number(num2);
+
+// if (num1 === 2 * num2) {
+//     console.log("2");
+// } else {
+//     console.log("noto'g'ri son kiritdingiz");
+// }
+
+
+// 30.Birinchi son ikkinchisidan kichik bo‘lsa va ikkinchi son toq bo‘lsa, -10 chiqarilsin.
+
+
+// let num1 = +prompt("Birinchi sonni kiriting");
+// let num2 = +prompt("Ikkinchi sonni kiriting");
+
+// if (num1 < num2 && num2 % 2 !== 0) {
+//     console.log("-10");
+// } else {
+//     console.log("noto'g'ri son kiritdingiz");
+// }
+
+
+// 31.Son musbat va 3 ga bo‘linadigan bo‘lsa, 3 chiqarilsin, aks holda 0 chiqarilsin.
+
+// let num = +prompt("Birinchi sonni kiriting");
+
+// if (num > 0 && num % 3 === 0) {
+//     console.log("3");
+// } else {
+//     console.log("0");
+// }
+
+
+// 32.Agar son manfiy va 2 ga bo‘linadigan bo‘lsa, -2 chiqarilsin.
+
+
+// let num = +prompt("Birinchi sonni kiriting");
+
+// if (num < 0 && num % 2 === 0) {
+//     console.log("-2");
+// } else {
+//     console.log("to'g'ri javob kiriting");
+// }
+
+// 33.Son musbat va 5 ga bo‘linadigan bo‘lsa, uning 5 ga bo‘linmasidan qolgan qismi chiqarilsin.
+
+
+// let num = +prompt("Sonni kiriting: ");
+
+// if (num > 0 && num % 5 === 0) {
+//     console.log(num % 5);
+// } else {
+//     console.log("noto'g'ri son kiritdingiz");
+// }
+
+
+// 34.Agar son musbat va 10 dan kichik bo‘lsa, uning kvadrati chiqarilsin.
+
+
+// let num = prompt("Sonni kiriting");
+
+// if (num > 0 && num <= 10) { 
+//     console.log(num * num);
+// } else {
+//     console.log("noto'g'ri");
+// }
+
+// 35.Son juft yoki 4 ga bo‘linadigan bo‘lsa, 1 chiqarilsin.
+
+
+// let num = prompt("Sonni kiriting");
+
+// if (num % 2 === 0 || num & 4 === 0) { 
+//     console.log("1");
+// } else {
+//     console.log("noto'g'ri");
+// }
+
+
+// 36.Agar son toq va 15 dan katta bo‘lsa, 15 chiqarilsin.
+
+
+// let num = prompt("Sonni kiriting");
+
+// if (num % 2 !== 0 && num > 15) { 
+//     console.log("15 dan katta");
+// }else{
+//     console.log("noto'g'ri");
+    
+// }
+
+
+
+// 37.Son 2 va 3 ga bo‘linadigan bo‘lsa, 6 chiqarilsin, aks holda 1 chiqarilsin.
+
+
+// let num = +prompt("Sonni kiriting");
+
+// if (num % 2 === 0 && num % 3 === 0) {
+//     console.log(6);
+// } else {
+//     console.log(1); 
+// }
+
+
+// 38.Agar son musbat va 7 ga bo‘linadigan bo‘lsa, 14 chiqarilsin.
+
+// let num = +prompt("Sonni kiriting");
+
+// if (num > 0 && num % 7 === 0) {
+//     console.log("14");
+    
+// }else{
+//     console.log("noto'g'ri");
+    
+// }
+
+// 39.Son musbat yoki 0 bo‘lsa, 0 chiqarilsin, aks holda -5 chiqarilsin.
+
+// let num = +prompt("Sonni kiriting");
+
+// if (num > 0 || num == 0) {
+//     console.log("0");
+    
+// }else{
+//     console.log("-5");
+    
+// }
+
+
+// 40.Son manfiy va 10 ga bo‘linmaydigan bo‘lsa, -3 chiqarilsin.
+
+
+// let num = +prompt("Sonni kiriting");
+
+// if (num < 0 && num % 10 !== 0) {
+//     console.log("-3");
+    
+// }else{
+//     console.log("noto'g'ri son ");
+    
+// }
